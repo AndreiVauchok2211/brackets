b931bf0280509dd2a6d55df825dd1ff6e256bdcf
AndreiVauchok2211 <vauchokandrei@gmail.com>
2023-02-28 00:21:19 +0300

    feat: brackets task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
-}
+  let result = [];
+
+  for (let br of str) {
+    let bracketsConfigIndex = bracketsConfig.indexOf(br);
+
+    if (bracketsConfigIndex === -1) {
+      continue;
+    }
+
+    if (bracketsConfigIndex % 2 === 0) {
+      result.push(bracketsConfigIndex + 1);
+    } else {
+      if (result.pop() !== bracketsConfigIndex) {
+        return false;
+      }
+    }
+  }
+  return result.length === 0;
+};
